4c313f0a0aba93035407eca2250850d34f37d556
giabao1511 <giabao712411@gmail.com>
2022-07-25 23:48:19 +0700

    fix grid

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -4,8 +4,8 @@ import propTypes from 'prop-types'
 const Grid = (props) => {
     const style = { gap: props.gap ? `${props.gap}px` : '0' }
     const col = `grid-col-${props.col}`
-    const mdCol = props.mdCol && `grid-md-${props.mdCol}`
-    const smCOl = props.smCol && `grid-sm-${props.smCol}`
+    const mdCol = props.mdCol && `grid-col-md-${props.mdCol}`
+    const smCOl = props.smCol && `grid-col-sm-${props.smCol}`
 
     return (
         <div className={`grid ${col} ${smCOl} ${mdCol}`} style={style}>
